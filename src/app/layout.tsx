6a0c0e3b8f237f772483b4d6851d9fb23574df35
Scofieldfree <kaofeesgf@gmail.com>
2024-09-25 15:51:39 +0800

    update:add title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   icons: { icon: "/favicon.ico" },
-  title: "ArtifAI",
+  title: "NeuroArt",
   description: "Generate Art NFT",
 };
 
