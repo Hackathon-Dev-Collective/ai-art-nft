74c88baafea847b259f4f9e50f743b3150438f09
Scofieldfree <kaofeesgf@gmail.com>
2024-10-02 23:25:09 +0800

    update:优化

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ export const  getCidUrl= (cid:string)=>{
     return `https://gateway.pinata.cloud/ipfs/${cid}`
 }
 
-export const shortenAddress = (address: string): string=> {
+export const shortenAddress = (address: string): string => {
   if (!address) return '';
   return `${address.substring(0, 6)}...${address.substring(address.length - 4)}`;
 }
